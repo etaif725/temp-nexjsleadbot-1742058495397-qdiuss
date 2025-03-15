47305147722fc8a3f9d8860e2833b0cd0d7eac33
ETAI FISHLER <142711245+etaif725@users.noreply.github.com>
2025-03-15 12:09:35 -0500

    Copy src/components/index.ts from nexjsleadbot

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+import Icons from './global/icons'
+import Container from './global/Container'
+import Wrapper from './global/Wrapper'
+
+import Navbar from './navigation/Navbar'
+import Footer from './navigation/Footer'
+
+export {
+    Icons,
+    Container,
+    Wrapper,
+    Navbar,
+    Footer
+}
